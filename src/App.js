e04accb015be0a7ceb6802958c190357c15bf8df
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-02-23 15:01:22 +0100

    maked todo as object class and isDone in ui

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ function App() {
   const [todos, setTodos] = useState([])
   const inputRef = useRef()
 
-  const addTodoHandler = () => {
-    const textInput = inputRef.current.value
-    setTodos([...todos, textInput])
+  const handleAddTodo = () => {
+    const todoTxt = inputRef.current.value
+    const todoObj = { name: todoTxt, isDone: false }
+
+    setTodos([...todos, todoObj])
     inputRef.current.value = ''
+  }
+
+  const handleIsDoneTodo = (index) => {
+    const newTodos = [...todos]
+    newTodos[index].isDone = !newTodos[index].isDone
+    setTodos(newTodos)
   }
 
   return (
@@ -16,10 +24,37 @@ function App() {
       <h2>To Do List</h2>
       <div className="to-do-container">
         <ul>
-          {todos.map((todo) => { return (<li>{todo}</li>) })}
+          {/* {todos.map(({ name }) => { return (<li>{name}</li>) })} */}
+          {
+            // todos.map(
+            //   (todo, index) => {
+            //     return (  // index as key is not the best practice, but here is ok
+            //       <li
+            //         className={todo.isDone ? "done" : ""}
+            //         key={index}
+            //         onClick={() => handleIsDoneTodo(index)}>
+            //         {todo.name}
+            //       </li>
+            //     )
+            //   }
+            // )
+
+            todos.map(
+              ({name, isDone}, index) => {
+                return (  // index as key is not the best practice, but here is ok
+                  <li
+                    className={isDone ? "done" : ""}
+                    key={index}
+                    onClick={() => handleIsDoneTodo(index)}>
+                    {name}
+                  </li>
+                )
+              }
+            )
+          }
         </ul>
         <input ref={inputRef} placeholder='Enter todo...' />
-        <button onClick={addTodoHandler}>Add</button>
+        <button onClick={handleAddTodo}>Add</button>
       </div>
     </div>
   );
